eeadab0e025895332d064990a4b6ed45c5e29afc
Raboni <raboni77@gmail.com>
2019-04-02 11:26:08 +0200

    Expnaded modal images
    added support for multiple modal images
    removed modal image from photo

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,10 +2,15 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
+var img = document.getElementsByClassName('myImg');
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function()
+for (var i = 0; i < img.length; i++)
+{
+    // Here we have the same onclick
+    img[i].onclick = clickImage;
+}
+function clickImage()
 {
 	modal.style.display = "block";
 	modalImg.src = this.src;
@@ -13,10 +18,10 @@ img.onclick = function()
 }
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function()
+modal.onclick = function()
 { 
 	modal.style.display = "none";
 }
